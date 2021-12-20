b10ef0bc0c813eb70e65f9e5d5f53e19710980af
Ahmad Madany <ahmad.madany.01@gmail.com>
2021-12-20 19:45:49 +0200

    Edit script.js to work with UI

--- script.js
+++ script.js
@@ -2,7 +2,47 @@
 
 const RPS = ['rock', 'paper', 'scissors']
 const numRounds = 3
+const scores = [0, 0, 0] // [tie, user, computer]
+let curRound = 0
 
+const roundElt = document.querySelector('.round .round-number')
+const userScoreElt = document.querySelector('.user-score .score-value')
+const ComputerScoreElt = document.querySelector('.computer-score .score-value')
+const choiceBtns = document.querySelectorAll('.choice-btn')
+const logElt = document.querySelector('.log')
+const resultElt = document.querySelector('.result')
+
+/**
+ * returns 0 or 1 or 2:
+ * 0 if tie
+ * 1 if choiceA is the winner
+ * 2 if choiceB is the winner
+ */
+const getRoundResult = function (choiceA, choiceB) {
+	const idxA = RPS.indexOf(choiceA.toLowerCase())
+	const idxB = RPS.indexOf(choiceB.toLowerCase())
+
+	// [Rock, Paper, Scissor] each choice win vs. the previous choice,
+	// fail vs. the next choice, tie with anything else
+	// so determine the result depending on the 'distance' between the two choices
+	return (idxA - idxB + RPS.length) % RPS.length
+}
+
+const getRoundMsg = function (choiceA, choiceB, roundResult) {
+	switch (roundResult) {
+		case 0:
+			return `Tie! ${choiceA} tie with ${choiceB}`
+		case 1:
+			return `You Win! ${choiceA} beats ${choiceB}`
+		case 2:
+			return `You Lose! ${choiceB} beats ${choiceA}`
+		default:
+			throw new Error(
+				`invalid roundResult ${roundResult}, ` +
+				`must be 0 (tie), 1 (choiceA wins), or 2 (choiceB wins)`
+			)
+	}
+}
 
 const computerPlay = function () {
 	const randIdx = Math.floor(Math.random() * RPS.length)
@@ -10,84 +50,68 @@ const computerPlay = function () {
 	return choice[0].toUpperCase() + choice.slice(1)
 }
 
-const userPlay = function () {
-	while (1) {
-		const choice = prompt(
-			`Choose one of [${RPS.join(', ')}], case insensitive`
-		)?.trim()
+const restartGame = function () {
+	scores.fill(0)
+	curRound = 0
 
-		if (choice && RPS.includes(choice.toLowerCase())) return choice
+	choiceBtns.forEach(btn => btn.disabled = false)
 
-		if (choice == undefined) console.error('your choice was empty, try again')
-		else console.error(
-			`invalid choice "${choice}", choose a valid one from [${RPS.join(' ')}]`
-		)
-	}
+	resultElt.classList.add('hidden')
+	resultElt.classList.remove('win', 'lose', 'tie')
+	logElt.classList.remove('win', 'lose', 'tie')
+
+	;[logElt, resultElt].forEach(elt => elt.textContent = '')
+	userScoreElt.textContent = ComputerScoreElt.textContent = 0
+	roundElt.textContent = 1
 }
 
-const game = function () {
+const finalizeGame = function () {
+	const [_, userScore, computerScore] = scores
 
-	/**
-	 * returns 0 or 1 or 2:
-	 * 0 if tie
-	 * 1 if choiceA is the winner
-	 * 2 if choiceB is the winner
-	 */
-	const getRoundResult = function (choiceA, choiceB) {
-		const idxA = RPS.indexOf(choiceA.toLowerCase())
-		const idxB = RPS.indexOf(choiceB.toLowerCase())
+	resultElt.classList.remove('hidden')
 
-		// [Rock, Paper, Scissor] each choice win vs. the previous choice,
-		// fail vs. the next choice, tie with anything else
-		// so determine the result depending on the 'distance' between the two choices
-		return (idxA - idxB + RPS.length) % RPS.length
+	if (userScore === computerScore) {
+		resultElt.classList.add('tie')
+		resultElt.textContent = 'Tie!'
+	}
+	else if (userScore > computerScore) {
+		resultElt.classList.add('win')
+		resultElt.textContent = 'You Win! 🏆'
+	}
+	else {
+		resultElt.classList.add('lose')
+		resultElt.textContent = 'You Lose! 💥'
 	}
 
-
-	const getRoundMsg = function (choiceA, choiceB, roundResult) {
-		switch (roundResult) {
-			case 0:
-				return `Tie! ${choiceA} tie with ${choiceB}`
-			case 1:
-				return `You Win! ${choiceA} beats ${choiceB}`
-			case 2:
-				return `You Lose! ${choiceB} beats ${choiceA}`
-			default:
-				throw new Error(
-					`invalid roundResult ${roundResult}, ` +
-					`must be 0 (tie), 1 (choiceA wins), or 2 (choiceB wins)`
-				)
+	choiceBtns.forEach(
+		btn => {
+			btn.disabled = true
 		}
-	}
-
-
-	const scores = [0, 0, 0] // [tie, user, computer]
-
-	for (let i = 0; i < numRounds; i++) {
-		const userChoice = userPlay()
-		const computerChoice = computerPlay()
-
-		const result = getRoundResult(userChoice, computerChoice)
-		scores[result]++
-
-		console.log(getRoundMsg(userChoice, computerChoice, result))
-
-	}
-
-	console.log('\n\n')
-	console.log('Result:')
-
-	const [numTies, userScore, computerScore] = scores
-
-	if (userScore === computerScore) console.log('%cTie!', 'color: royalblue;')
-	else if (userScore > computerScore) console.log('%cYou Win!', 'color: greenyellow;')
-	else console.log('%cYou Lose!', 'color: orangered;')
-
-	console.log(`${numRounds} rounds:`)
-	console.log(`\tyour score is ${userScore}`)
-	console.log(`\tcomputer score is ${computerScore}`)
-	console.log(`\twith ${numTies} tie(s)`)
+	)
 }
 
+const playRound = function (userChoice) {
+	curRound++
 
-game()
+	const computerChoice = computerPlay()
+	const result = getRoundResult(userChoice, computerChoice)
+	scores[result]++
+
+	const msg = getRoundMsg(userChoice, computerChoice, result)
+
+	logElt.textContent = msg
+	logElt.classList.remove('win', 'lose', 'tie')
+	logElt.classList.add(['tie', 'win', 'lose'][result])
+
+	roundElt.textContent = curRound;
+	[userScoreElt.textContent, ComputerScoreElt.textContent] = [scores[1], scores[2]]
+
+	if (curRound !== numRounds) return
+	finalizeGame()
+}
+
+choiceBtns.forEach(
+	btn => btn.addEventListener('click', e => playRound(e.target.dataset.choice))
+)
+document.querySelector('.restart').addEventListener('click', restartGame)
+document.querySelector('.num-rounds').textContent = numRounds
